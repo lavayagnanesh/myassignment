d5794831a8cfe3da9ed06eb18d85785f6e42b502
lavayagnanesh <134921078+lavayagnanesh@users.noreply.github.com>
2023-06-27 15:32:28 +0530

    Update FindSecondLargest.java

--- FindSecondLargest.java
+++ FindSecondLargest.java
@@ -7,9 +7,9 @@ public class FindSecondLargest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] data = {3,2,11,4,6,7};
-		//int len=data.length;
+		int len=data.length-2;
 		Arrays.sort(data);
-		System.out.println(data[4]);
+		System.out.println(data[len]);
 		
 		
 		
